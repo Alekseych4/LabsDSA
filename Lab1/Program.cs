9afb086f1635c4bd151f2100c86c8afb2d5071ab
Alekseych4 <alekseych@list.ru>
2020-06-25 18:27:08 +0300

    Lab 5 setting

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -27,80 +27,102 @@ namespace Lab1
                 }
             } while (true);
 
-
             Console.WriteLine("Команды для использования программы:");
+            Console.WriteLine("ADD  добавление элемента");
+            Console.WriteLine("DEL  удаление элемента");
+            Console.WriteLine("SHOW  состояние списка");
+            Console.WriteLine("GET поиск элемента");
+            Console.WriteLine("ISEMPTY  пустота");
+            Console.WriteLine("ISFULL  полнота");
             Console.WriteLine("q  выход из программы");
             Console.WriteLine();
-            random = new Random();
-            Console.WriteLine("Инициирован датчик псевдослучайных чисел");
-            
-            while (true)
+
+            var command = Console.ReadLine();
+            do
             {
-                var randomNum = random.Next(1, 101);
-                if (randomNum % 2 == 0)
+                if (command.Equals("q"))
                 {
-                    AddElements();
-                }
-                else
-                {
-                    DeleteElements();
-                }
-                
-                Console.WriteLine();
-                Console.WriteLine("Закончить выполнение? (для выхода введите q и нажмите Enter, для продолжения нажмите Enter)");
-                var command =  Console.ReadLine();
-                if (command == "q")
-                {
-                    _myList.Dispose();
                     break;
                 }
-            }
-        
+                CommandRecognizer(command);
+                command = Console.ReadLine();
+            } while (true);
         }
-
-        private static void DeleteElements()
+        
+         private static void CommandRecognizer(string command)
         {
-            var delElements = random.Next(1, 4);
-            Console.WriteLine("Операция: удаление");
-            Console.WriteLine($"Количество удаляемых элементов: {delElements}");
-            for (int i = 0; i < delElements; i++)
+            switch (command)
             {
-                var el = _myList.remove();
-                if (el != '\0')
-                {
-                    Console.WriteLine("Удаленный элемент: " + el);
-                    Console.Write("Состояние очереди после удаления: ");
+                case "ADD":
+                    if (_myList.isFull()) Console.WriteLine("Список заполнен");
+                    else
+                    {
+                        var data = int.Parse(Console.ReadLine());
+                        if (_myList.add(data))
+                        {
+                            Console.WriteLine("Данные успешно записаны");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не удалось записать данные");
+                        }
+                    }
+
+                    break;
+                
+                case "SHOW":
                     _myList.showState();
                     Console.WriteLine();
-                }
-                else
-                {
-                    Console.WriteLine("Очередь пуста!");
                     break;
-                }
-            }
-        }
-
-        private static void AddElements()
-        {
-            var addElements = random.Next(1, 4);
-            Console.WriteLine("Операция: добавление");
-            Console.WriteLine($"Количество добавляемых элементов: {addElements}");
-            for (int i = 0; i < addElements; i++)
-            {
-                if (_myList.isFull())
-                {
-                    Console.WriteLine("Очередь заполнена!");
-                    _myList.showState();
+                
+                case "DEL":
+                    var i = _myList.remove(int.Parse(Console.ReadLine()));
+                    if (i != -1)
+                    {
+                        Console.WriteLine($"Удаленный элемент имел индекс {i}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нет такого элемента");
+                    }
                     break;
-                }
-
-                var charCode = random.Next(65, 91);
-                char sym = (char) charCode;
-                _myList.add(sym);
-                _myList.showState();
-                Console.WriteLine();
+                case "GET":
+                    var index = _myList.getItemIndex(int.Parse(Console.ReadLine()));
+                    if (index != -1)
+                    {
+                        Console.WriteLine($"Данный элемент имеет индекс {index}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нет такого элемента");
+                    }
+                    
+                    break;
+                case "ISEMPTY":
+                    if (_myList.isEmpty())
+                    {
+                        Console.WriteLine("Список пуст");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Список не пуст");
+                    }
+                    break;
+                case "ISFULL":
+                    if (_myList.isFull())
+                    {
+                        Console.WriteLine("Список заполнен");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Список не заполнен");
+                    }
+                    break;
+                default: 
+                    Console.WriteLine("No such command");
+                    break;
             }
+            
         }
 
     }
