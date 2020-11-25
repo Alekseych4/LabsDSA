f784f5c9b0dc7754f8dad1320ae33375b7356b2e
Alekseych4 <alekseych@list.ru>
2020-11-25 12:13:34 +0300

    Lab 15 . Stack or list?

---
+++ Lab_13_14/DataStructure.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab_13_14
+{
+    public class DataStructure<T>
+    {
+        private T _node;
+        private DataStructure<T> _next;
+
+        public T Node
+        {
+            get => _node;
+            set => _node = value;
+        }
+
+        public DataStructure<T> Next
+        {
+            get => _next;
+            set => _next = value;
+        }
+    }
+}
--- Lab_13_14/Program.cs
+++ Lab_13_14/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lab1;
 
 namespace Lab_13_14
 {
@@ -7,7 +8,7 @@ namespace Lab_13_14
         private static int[] array;
         private static ulong equalityCounter;
         private static ulong moveCounter;
-        
+
         public static void Main(string[] args)
         {
             Console.WriteLine("RAN  создать массив со случайными числами");
@@ -32,252 +33,91 @@ namespace Lab_13_14
             }
         }
 
-        private static int[] randomArray(int num)
+        private static int[] randomArray(int num, int minInclusive, int maxExclusive)
         {
             Random random = new Random();
             var array = new int[num];
             
             for (int i = 0; i < num; i++)
             {
-                array[i] = random.Next(-100, 1000);
+                array[i] = random.Next(minInclusive, maxExclusive);
             }
 
             return array;
         }
 
-        private static int[] bubbleSort(int [] inputArray)
+        private static int[] arrayForPocketSort(int n)
         {
-            moveCounter = 0;
-            equalityCounter = 0;
-
-            for (int i = inputArray.Length; i > 0; i--)
+            var key = 0;
+            var array = new int[n];
+            
+            for (int i = n - 1; i >= 0; i--)
             {
-                for (int j = 0; j < i - 1; j++)
-                {
-                    equalityCounter++;
-                    
-                    if (inputArray[j] > inputArray[j + 1])
-                    {
-                        var swap = inputArray[j + 1];
-                        inputArray[j + 1] = inputArray[j];
-                        inputArray[j] = swap;
-                        moveCounter++;
-                    }
-                }
-                
+                array[i] = key;
+                key++;
             }
-            Console.WriteLine($"Количество сравнений в сортировке пузырька (обмена): {equalityCounter}");
-            Console.WriteLine($"Количество перестановок в сортировке пузырька (обмена): {moveCounter}");
 
-            return inputArray;
+            return array;
         }
 
-        private static int[] insertionSort(int[] inputArray)
+        private static int[] sameKeysAndIndices(int[] inputArray)
         {
-            moveCounter = 0;
-            equalityCounter = 0;
-            
-            for (int i = 0; i < inputArray.Length - 1; i++)
-            {
-                var j = i;
-                var temp = inputArray[i + 1];
-
-                
-                while (j >= 0 && inputArray[j] > temp)
-                {
-                    equalityCounter++;
-                    inputArray[j + 1] = inputArray[j];
-                    j--;
-                    moveCounter++;
-                }
-
-                inputArray[j + 1] = temp;
-            }
-            Console.WriteLine($"Количество сравнений в сортировке вставками: {equalityCounter}");
-            Console.WriteLine($"Количество перестановок в сортировке вставками: {moveCounter}");
-
-            return inputArray;
-        }
-
-        private static int[] selectionSort(int[] inputArray)
-        {
-            moveCounter = 0;
-            equalityCounter = 0;
-            
+            var newArray = new int[inputArray.Length];
             for (int i = 0; i < inputArray.Length; i++)
             {
-                int min = i;
-                
-                for (int j = i + 1; j < inputArray.Length; j++)
-                {
-                    if (inputArray[min] > inputArray[j])
-                    {
-                        equalityCounter++;
-                        min = j;
-                    }
-                }
-
-                var temp = inputArray[i];
-                inputArray[i] = inputArray[min];
-                inputArray[min] = temp;
-                moveCounter++;
+                newArray[inputArray[i]] = inputArray[i];
             }
-            
-            Console.WriteLine($"Количество сравнений в сортировке выбором: {equalityCounter}");
-            Console.WriteLine($"Количество перестановок в сортировке выбором: {moveCounter}");
-            
-            return inputArray;
+
+            return newArray;
         }
 
-        private static int[] advancedInsertionSort(int[] inputArray)
+        private static int[] sameArray(int[] inputArray)
         {
-            moveCounter = 0;
-            equalityCounter = 0;
-            
-            var steps = new int[] { 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191, 16383, 32767, 65535, 131071};
-            var stepsCount =  (int)Math.Log(inputArray.Length, 2) - 1;
-            Console.WriteLine(stepsCount);
-            
-            for (int i = stepsCount; i >= 0; i--)
+            for (int i = 0; i < inputArray.Length; i++)
             {
-                var k = steps[i];
-                for (int j = k; j < inputArray.Length; j++)
-                {
-                    var temp = inputArray[j];
-                    var l = j - k;
-                    while (l >= 0 && temp < inputArray[l])
-                    {
-                        equalityCounter++;
-                        inputArray[l + k] = inputArray[l];
-                        l -= k;
-                        moveCounter++;
-                    }
+                var key = inputArray[i];
+                var temp = inputArray[key];
+                inputArray[key] = key;
+                inputArray[i] = temp;
 
-                    inputArray[l + k] = temp;
-                    
+                if (inputArray[i] != i)
+                {
+                    i--;
                 }
             }
-            
-            Console.WriteLine($"Количество сравнений в сортировке методом Шелла: {equalityCounter}");
-            Console.WriteLine($"Количество перестановок в сортировке методом Шелла: {moveCounter}");
 
             return inputArray;
         }
         
-        private static int[] quickSort(int[] inputArray, int leftStart, int rightStart)
+        public struct Item
         {
-            moveCounter = 0;
-            equalityCounter = 0;
-
-            int middle = inputArray[(rightStart + leftStart) / 2];
-            int leftSide = leftStart;
-            int rightSide = rightStart;
-
-            do
-            {
-            
-                while(inputArray[leftSide] < middle)
-                {
-                    equalityCounter++;
-                    leftSide++;
-                }
-
-                while (inputArray[rightSide] > middle)
-                {
-                    equalityCounter++;
-                    rightSide--;
-                }
-
-                if (leftSide <= rightSide)
-                {
-                    equalityCounter++;
-                    var temp = inputArray[leftSide];
-                    inputArray[leftSide] = inputArray[rightSide];
-                    inputArray[rightSide] = temp;
-                    moveCounter++;
-
-                    leftSide++;
-                    rightSide--;
-                }
-
-            } while (leftSide < rightSide);
-
-            if (leftStart < rightSide)
-            {
-                quickSort(inputArray, leftStart, rightSide);
-            }
-
-            if (rightStart > leftSide)
-            {
-                quickSort(inputArray, leftSide, rightStart);
-            }
-            
-            // Console.WriteLine($"Количество сравнений в быстрой сортировке: {equalityCounter}");
-            // Console.WriteLine($"Количество перестановок в быстрой сортировке: {moveCounter}");
-
-            return inputArray;
+            public int firstKey;
+            public int lastKey;
         }
 
-        private static int[] heapsort(int[] inputArray)
+        private static int[] bucketSort(int[] inputArray)
         {
-            moveCounter = 0;
-            equalityCounter = 0;
-
-            var curr = (inputArray.Length - 1) / 2;
-            var last = inputArray.Length - 1;
-            
-            while (curr >= 0)
+            var arr = new MyDynamicList<int>[inputArray.Length];
+            for (int i = 0; i < arr.Length; i++)
             {
-                maxHeapify(curr, inputArray, inputArray.Length);
-                curr--;
-            }
-            printArray(inputArray);
-
-            for (int i = inputArray.Length - 1; i > 0; i--)
-            {
-                var temp = inputArray[0];
-                inputArray[0] = inputArray[i];
-                inputArray[i] = temp;
+                var key = inputArray[i];
                 
-                maxHeapify(0, inputArray, i);
-            }
-            
-            Console.WriteLine($"Количество сравнений в пирамидальной сортировке: {equalityCounter}");
-            Console.WriteLine($"Количество перестановок в пирамидальной сортировке: {moveCounter}");
-            
-            return inputArray;
-        }
-
-        private static void maxHeapify(int curr, int[] inputArray, int size)
-        {
-            var left = 2 * curr + 1;
-            var right = 2 * curr + 2;
-            var mem = curr;
-
-            if (right < size)
-            {
-                if (inputArray[left] > inputArray[right])
+                if (arr[key] == null)
                 {
-                    mem = inputArray[left] > inputArray[curr] ? left : curr;
+                    arr[key] = new MyDynamicList<int>();
+                    arr[key].add(inputArray[i]);
                 }
                 else
                 {
-                    mem = inputArray[right] > inputArray[curr] ? right : curr;
+                    arr[key].add(inputArray[i]);
                 }
             }
-            else if (left < size && inputArray[curr] < inputArray[left])
-            {
-                mem = left;
-            }
 
-            if (mem != curr)
+            for (int i = 0; i < arr.Length; i++)
             {
-                var temp = inputArray[curr];
-                inputArray[curr] = inputArray[mem];
-                inputArray[mem] = temp;
-                
-                maxHeapify(mem, inputArray, size);
+                inputArray[i] = arr[i].remove()
             }
+            return inputArray;
         }
 
         private static void commandRecognizer(string command)
@@ -312,51 +152,7 @@ namespace Lab_13_14
                         printArray(result);
                     }
                     break;
-                case "INS":
-                    if (array.Length > 0)
-                    {
-                        int[] arrayCopy = new int[array.Length];
-                        Array.Copy(array, arrayCopy, array.Length);
-                        var result = insertionSort(arrayCopy);
-                        printArray(result);
-                    }
-                    break;
-                case "SES":
-                    if (array.Length > 0)
-                    {
-                        int[] arrayCopy = new int[array.Length];
-                        Array.Copy(array, arrayCopy, array.Length);
-                        var result = selectionSort(arrayCopy);
-                        printArray(result);
-                    }
-                    break;
-                case "SHELL":
-                    if (array.Length > 0)
-                    {
-                        int[] arrayCopy = new int[array.Length];
-                        Array.Copy(array, arrayCopy, array.Length);
-                        var result = advancedInsertionSort(arrayCopy);
-                        printArray(result);
-                    }
-                    break;
-                case "QS":
-                    if (array.Length > 0)
-                    {
-                        int[] arrayCopy = new int[array.Length];
-                        Array.Copy(array, arrayCopy, array.Length);
-                        var result = quickSort(arrayCopy, 0, arrayCopy.Length - 1);
-                        printArray(result);
-                    }
-                    break;
-                case "HS":
-                    if (array.Length > 0)
-                    {
-                        int[] arrayCopy = new int[array.Length];
-                        Array.Copy(array, arrayCopy, array.Length);
-                        var result = heapsort(arrayCopy);
-                        printArray(result);
-                    }
-                    break;
+                
             }
         }
 
