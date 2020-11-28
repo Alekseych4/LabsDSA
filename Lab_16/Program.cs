5da8e4db1d04a094d1c7e6709549147d9322770e
Alekseych4 <alekseych@list.ru>
2020-11-28 07:33:55 +0300

    Lab 17 pre pass

--- Lab_16/Program.cs
+++ Lab_16/Program.cs
@@ -5,13 +5,34 @@ namespace Lab_16
     internal class Program
     {
         private static string[] keys = new[] {"begin;", "!end", "do", "start", "for", "else", "as", "var", "IF", "integer"};
-        private const int Length = 10;
-        private static string[] hashTable = new string [10];
+        private static int tableLength;
+        private static string[] hashTable;
+        private static long equalityCounter;
+        private static long totalOperations;
         public static void Main(string[] args)
         {
-            Console.WriteLine("H  создать хэш-таблицу и вывести её на экран");
+            do
+            {
+                Console.WriteLine("Введите длину хэш-таблицы");
+                Console.WriteLine();
+                var lengthStr = Console.ReadLine();
+                if (int.TryParse(lengthStr, out var length))
+                {
+                    if (length > 0)
+                    {
+                        tableLength = length;
+                        hashTable = new string[length];
+                        break;
+                    }
+                }
+            } while (true);
+            
+            Console.WriteLine("L  ввести длину хэш-таблицы");
+            Console.WriteLine("A  добавить элемент");
             Console.WriteLine("F  поиск элемента в хэш-таблице");
             Console.WriteLine("C  хэш-код слова");
+            Console.WriteLine("P  вывести на экран");
+            Console.WriteLine("TO  количество всех сравнений за время использования");
             Console.WriteLine("EXT  выход");
 
             while (true)
@@ -38,12 +59,32 @@ namespace Lab_16
             
             //Console.WriteLine($"{key} = {sum}");
 
-            return sum % Length;
+            return sum % tableLength;
         }
 
-        private static void putInTable(int hashCode, string el)
+        private static bool putInTable(int hashCode, string el)
         {
-            hashTable[hashCode] = el;
+            equalityCounter = 0;
+            equalityCounter++;
+            if (string.IsNullOrEmpty(hashTable[hashCode]))
+            {
+                hashTable[hashCode] = el;
+                return true;
+            }
+
+            for (int k = 1; k < tableLength; k++)
+            {
+                var j = (hashCode + k) % tableLength;
+                
+                equalityCounter++;
+                if (string.IsNullOrEmpty(hashTable[j]))
+                {
+                    hashTable[j] = el;
+                    return true;
+                }
+            }
+            
+            return false;
         }
 
         private static void printTable()
@@ -57,11 +98,25 @@ namespace Lab_16
 
         private static bool searchKey(string key)
         {
+            equalityCounter = 0;
             var hashSearch = hash(key);
+            equalityCounter++;
             if (hashTable[hashSearch].Equals(key))
             {
                 Console.WriteLine($"Элемент находится в {hashSearch} ячейке");
                 return true;
+            }
+            
+            for (int k = 1; k < tableLength; k++)
+            {
+                var j = (hashSearch + k) % tableLength;
+            
+                equalityCounter++;
+                if (hashTable[j].Equals(key))
+                {
+                    Console.WriteLine($"Элемент находится в {j} ячейке");
+                    return true;
+                }
             }
 
             return false;
@@ -71,14 +126,41 @@ namespace Lab_16
         {
             switch (command)
             {
-                case "H":
-                    foreach (var el in keys)
+                case "L":
+                    do
                     {
-                        var hashCode = hash(el);
-                        putInTable(hashCode, el);
+                        Console.WriteLine("Введите длину хэш-таблицы");
+                        var lengthStr = Console.ReadLine();
+                        if (int.TryParse(lengthStr, out var length))
+                        {
+                            if (length > 0)
+                            {
+                                totalOperations = 0;
+                                equalityCounter = 0;
+                                tableLength = length;
+                                hashTable = new string[length];
+                                break;
+                            }
+                        }
+                    } while (true);
+                    break;
+                case "A":
+                    Console.WriteLine("Введите элемент:");
+                    var e = Console.ReadLine();
+
+                    var code = hash(e);
+                    if (putInTable(code, e))
+                    {
+                        Console.WriteLine("Элемент успешно добавлен");
+                        totalOperations += equalityCounter;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Элемент не добавлен, таблица заполнена");
                     }
                     
-                    printTable();
+                    Console.WriteLine($"Кол-во сравнений: {equalityCounter}");
+                    
                     break;
                 case "F":
                     Console.WriteLine("Введите ключ для поиска");
@@ -88,11 +170,19 @@ namespace Lab_16
                         Console.WriteLine("Данный элемент отсутствует в хэш-таблице");
                     }
                     
+                    Console.WriteLine($"Кол-во сравнений: {equalityCounter}");
+                    totalOperations += equalityCounter;
                     break;
                 case "C":
                     var input = Console.ReadLine();
                     var h = hash(input);
                     Console.WriteLine($"Хэш-код значения {input}: {h}");
+                    break;
+                case "P":
+                    printTable();
+                    break;
+                case "TO":
+                    Console.WriteLine("Все сравнения: " + totalOperations);
                     break;
             }
         }
